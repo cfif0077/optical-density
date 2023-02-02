7925d232b7e4ab1efdab9ad81659ea96a19955fb
Alex <antoshih.a@gmail.com>
2023-02-02 14:55:30 +0300

    добавил и совместил фильтры

--- OP/main.py
+++ OP/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
-from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.filedialog import asksaveasfilename
 from tkinter import messagebox
 from PIL import Image, ImageTk, ImageEnhance
 import os
@@ -10,7 +10,7 @@ from numpy import asarray
 class Photo:
     def __init__(self):
         self.flag = False
-        self.size = (640, 420)
+        self.size = (900, 420)
         self.rgb = None
 
     def out(self):
@@ -23,7 +23,8 @@ class Photo:
 
     def select(self):
         self.path = filedialog.askopenfilename(initialdir=os.getcwd())
-        self.image_clean = Image.open(self.path)
+        self.image_true_clean = Image.open(self.path)
+        self.image_clean = self.image_true_clean
         self.image = self.image_clean
         self.out()
 
@@ -33,11 +34,12 @@ class Photo:
         self.image.save(file)
 
     def resize(self, event):
+        self.size = (event.width, event.height)
         if self.flag:
-            self.size = (event.width, event.height)
             self.out()
 
     def out_image_not_clean(self):
+        # TODO: добавить расчет
         source = self.image_clean.split()
         r, g, b = 0, 1, 2
         error = v1.get()
@@ -57,15 +59,20 @@ class Photo:
         self.image = self.image_clean
         self.out()
 
-    def brightness(self, event):
-        image_for_brightness = ImageEnhance.Brightness(self.image_clean)
-        self.image = image_for_brightness.enhance(var_bright.get() + 1)
-        self.out()
+    def filter(self, event):
+        image_for_brightness = ImageEnhance.Brightness(self.image_true_clean)
+        image = image_for_brightness.enhance(var_bright.get() * 0.1 + 1)
 
-    def contrast(self, event):
-        image_for_contrast = ImageEnhance.Contrast(self.image_clean)
-        self.image = image_for_contrast.enhance(var_contrast.get() + 1)
-        self.out()
+        image_for_contrast = ImageEnhance.Contrast(image)
+        image = image_for_contrast.enhance(var_contrast.get() * 0.1+1)
+
+        image_for_sharpness = ImageEnhance.Sharpness(image)
+        image = image_for_sharpness.enhance(var_sharpness.get() + 1)
+
+        image_for_color_balance = ImageEnhance.Color(image)
+        self.image_clean = image_for_color_balance.enhance(var_color_balance.get() * 0.1 + 1)
+
+        checkbutton_changed()
 
 
 def b3(event):
@@ -74,11 +81,34 @@ def b3(event):
     checkbutton_changed()
 
 
+# окно настроек счета
+def checkbutton_changed(event=1):
+    if enabled.get() == 1:
+        if photo.rgb is not None:
+            photo.out_image_not_clean()
+        else:
+            error_not_rgb()
+            btn4.deselect()
+    else:
+        photo.out_image_clean()
+
+
+def help_click():
+    messagebox.showinfo("Тех. поддержка", "89119659493\n Александр")
+
+
+def error_not_rgb():
+    messagebox.showerror("Ошибка", "Выберите цвет")
+
+def calculation():
+    pass
+
+
 photo = Photo()
 
 root = Tk()  # Создание окна
 root.title("OP")
-root.geometry("640x560")
+root.geometry("900x560")
 
 frame_photo = Frame(root)
 frame_photo.pack(fill=BOTH, expand=True)
@@ -94,7 +124,7 @@ frame_setting_calculate.pack(fill=Y)
 
 
 # окно с фото
-canvas2 = Canvas(frame_photo, width=600, height=420)
+canvas2 = Canvas(frame_photo, width=900, height=420)
 canvas2.pack(fill=BOTH, expand=1)
 
 
@@ -112,30 +142,34 @@ btn_exit.pack(side=LEFT)
 # яркость
 bright = Label(frame_setting_photo, text="Яркость:", font="ariel 15 bold")
 bright.pack(side=LEFT)
-var_bright = IntVar()
-scale_bright = Scale(frame_setting_photo, from_=0, to=10, variable=var_bright,
-                     orient=HORIZONTAL, command=photo.brightness)
+var_bright = IntVar(value=0)
+scale_bright = Scale(frame_setting_photo, from_=-10, to=10, variable=var_bright,
+                     orient=HORIZONTAL, command=photo.filter)
 scale_bright.pack(side=LEFT)
 
 # Контрастность
-contrast = Label(frame_setting_photo, text="Контраст:", font="ariel 17 bold")
+contrast = Label(frame_setting_photo, text="Контраст:", font="ariel 15 bold")
 contrast.pack(side=LEFT)
-var_contrast = IntVar()
-scale_contrast = Scale(frame_setting_photo, from_=0, to=10, variable=var_contrast,
-                       orient=HORIZONTAL, command=photo.contrast)
+var_contrast = IntVar(value=0)
+scale_contrast = Scale(frame_setting_photo, from_=-10, to=10, variable=var_contrast,
+                       orient=HORIZONTAL, command=photo.filter)
 scale_contrast.pack(side=LEFT)
 
+# Резкость
+sharpness = Label(frame_setting_photo, text="Резкость:", font="ariel 15 bold")
+sharpness.pack(side=LEFT)
+var_sharpness = IntVar(value=0)
+scale_sharpness = Scale(frame_setting_photo, from_=-10, to=10, variable=var_sharpness,
+                        orient=HORIZONTAL, command=photo.filter)
+scale_sharpness.pack(side=LEFT)
 
-# окно настроек счета
-def checkbutton_changed(event=1):
-    if enabled.get() == 1:
-        if photo.rgb is not None:
-            photo.out_image_not_clean()
-        else:
-            error_not_rgb()
-            btn4.deselect()
-    else:
-        photo.out_image_clean()
+# Цветовой баланс
+color_balance = Label(frame_setting_photo, text="Цветовой баланс", font="ariel 15 bold")
+color_balance.pack(side=LEFT)
+var_color_balance = IntVar(value=0)
+scale_color_balance = Scale(frame_setting_photo, from_=-10, to=10, variable=var_color_balance,
+                            orient=HORIZONTAL, command=photo.filter)
+scale_color_balance.pack(side=LEFT)
 
 
 enabled = IntVar()
@@ -144,12 +178,14 @@ btn4 = Checkbutton(frame_setting_calculate, text="Проверка", width=8, fo
 btn4.pack(side=LEFT)
 
 v1 = IntVar()
-scale3 = Scale(frame_setting_calculate, from_=0, to=40, variable=v1, orient=HORIZONTAL, command=checkbutton_changed)
+scale3 = Scale(frame_setting_calculate, from_=0, to=50, variable=v1, orient=HORIZONTAL, command=checkbutton_changed)
 scale3.pack(side=LEFT)
 
 canvas3 = Canvas(frame_setting_calculate, width="40", height="40", relief=RIDGE, bd=2)
 canvas3.pack(side=LEFT)
 
+entry_calculation = Entry(frame_setting_calculate, width=9, font='ariel 15 bold')
+entry_calculation.pack(side=LEFT)
 
 main_menu = Menu()
 
@@ -158,15 +194,6 @@ file_menu.add_command(label="Открыть", command=photo.select)
 file_menu.add_command(label="Сохранить", command=photo.save)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=root.destroy)
-
-
-def help_click():
-    messagebox.showinfo("Тех. поддержка", "89119659493\n Александр")
-
-
-def error_not_rgb():
-    messagebox.showerror("Ошибка", "Выберите цвет")
-
 
 main_menu.add_cascade(label="File", menu=file_menu)
 main_menu.add_cascade(label="Тех. поддержка", command=help_click)
